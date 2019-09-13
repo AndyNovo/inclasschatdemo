f871a973ef44e93436ffff7e305c6baf87b26a61
junko <junk@novocin.com>
2019-09-13 14:47:16 +0000

    added stuff

--- api.php
+++ api.php
@@ -3,8 +3,27 @@
   //print_r($_POST);
   $verb = $_SERVER["REQUEST_METHOD"];
   
+  function handle_sql_errors($query, $error_message)
+{
+    echo '<pre>';
+    echo $query;
+    echo '</pre>';
+    echo $error_message;
+    die;
+}
+  
+  $dbhandle = new PDO("sqlite:db/chat.db") or die("Failed to open DB");
+  if (!$dbhandle) die ($error);
+  $dbhandle->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  
   if ($verb == "GET"){
-      echo "GET HERE";
+      $query = "SELECT * FROM msgs";
+      $statement = $dbhandle->prepare($query);
+      $statement->execute();
+      $results = $statement->fetchAll(PDO::FETCH_ASSOC);
+      header('HTTP/1.1 200 OK');
+      header('Content-Type: application/json');
+      echo json_encode($results);
   } else if ($verb == "POST"){
       $author = "anonymous";
       $content = "secret message";
@@ -14,8 +33,16 @@
       if (isset($_POST["content"])){
           $content = $_POST["content"];
       }
-      echo "$author: $content";
-  } else {
-      echo "USAGE GET or POST";
+      $query = "INSERT into msgs ('author', 'content') VALUES (\"".$author."\",\"".$content."\")";
+    try{
+      $statement = $dbhandle->query($query);
+      $statement->fetch(PDO::FETCH_ASSOC);
+     } catch(PDOException $e)
+    {
+        handle_sql_errors($statement, $e->getMessage());
+    }
+      header('HTTP/1.1 200 OK');
+      header('Content-Type: application/json');
+      echo json_encode("hmm");
   }
 ?>
