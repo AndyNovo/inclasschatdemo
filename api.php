2084727f332b4918ee3ab143afa821608dd59cf7
junko <junk@novocin.com>
2019-09-13 13:31:25 +0000

    api and index

---
+++ api.php
@@ -0,0 +1,21 @@
+<?php
+  // Open https://localhost/~ubuntu/index.php
+  //print_r($_POST);
+  $verb = $_SERVER["REQUEST_METHOD"];
+  
+  if ($verb == "GET"){
+      echo "GET HERE";
+  } else if ($verb == "POST"){
+      $author = "anonymous";
+      $content = "secret message";
+      if (isset($_POST["author"])){
+          $author = $_POST["author"];
+      }
+      if (isset($_POST["content"])){
+          $content = $_POST["content"];
+      }
+      echo "$author: $content";
+  } else {
+      echo "USAGE GET or POST";
+  }
+?>
